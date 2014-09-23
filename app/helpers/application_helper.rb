9ed7e450a8cc480d426400db4c9c3bd3bddba46a
Alexey Leontyev <leshka.xd@gmail.com>
2014-09-23 13:37:18 +0400

    Add a headline helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def headline(page_title)
+    app_title = "Ruby on Rails Tutorial Sample App"
+    unless page_title.empty? 
+      "#{app_title} | #{page_title}"
+    else
+      "#{app_title}"
+    end
+  end
 end
